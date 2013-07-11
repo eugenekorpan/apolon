b5097b0f992dd9a23b413198ba6e5c475f51b8f4
Kris Simpson <s.vitko@svitla.com>
2013-07-11 11:24:25 +0300

    delete some trash

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,4 @@ Apolon::Application.routes.draw do
   resources :books
   resources :users
   root :to => 'users#index'
-
-  resources :books do
-	  get :autocomplete_user_first_name, on: :collection
-	end
 end
